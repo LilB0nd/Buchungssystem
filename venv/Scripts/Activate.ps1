60aeaf2bddd097127b21e473147ff00c990a6bbf
LilB0nd <57658534+LilB0nd@users.noreply.github.com>
2021-07-05 21:28:17 +0200

    Venv

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\yvoda\OneDrive\Dokumente\GitHub\Buchungssystem\venv"
+$env:VIRTUAL_ENV="C:\Schule\Programmierpraktikum\Server\Buchungssystem\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\yvoda\OneDrive\Dokumente\GitHub\Buchungssystem\venv\Scripts\python.exe
+#!C:\Schule\Programmierpraktikum\Server\Buchungssystem\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
