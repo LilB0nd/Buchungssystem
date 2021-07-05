97af211f92de6484a6ed1fe3868c00d91d66dd5b
SnuggleUp <58076301+SnuggleUp@users.noreply.github.com>
2021-07-05 14:10:13 +0200

    Merge remote-tracking branch 'origin/master'

--- App_Buchung/models.py
+++ App_Buchung/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from App_Buchung.Views.booking import Booking
 from django.views import generic
+from App_Buchung.Models.Equipment_Models import Equipment
 # Create your models here.
 
+Equipment()
