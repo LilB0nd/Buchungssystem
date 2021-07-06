70f35dca142f2c561f835ed938a70078f876b680
CaptainDaraen <beste.laszlo@gmail.com>
2021-07-06 10:13:01 +0200

    Accountmodels.py changes to Usermodell

--- App_Buchung/Models/Accountmodels.py
+++ App_Buchung/Models/Accountmodels.py
@@ -1,23 +1,20 @@
 from django.db import models
-from django.core.exceptions import ValidationError
-class Accounts(models.Model):
-    Email = models.EmailField(max_length=254)
-    Username = models.CharField(max_length=30)
-    password = models.CharField(max_lenght=100)
-    Vorname = models.CharField(max_length=30)
-    Nachname = models.CharField(max_length=50)
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+class Profile(models.Model):
     Klasse = models.Choices[
-        ("dqi17", "DQI17"
-         "dqi18", "DQI18"
-         "dqi19", "DQI19"
-         "dqi20", "DQI20")
-    ]
+        (
+            "dqi18","DQI1818"
+            "dqi19","DQI19"
+            "dqi20","DQI20"
+            "dqi21","DQI21")]
     Einverstaendniserklaerung = models.BooleanField(default=0)
     Kursbelegung = models.BooleanField(default=0)
-    def __str__(self):
-        return self.Vorname
-    def clean(self):
-        if "@schule.bremen.de" not in self.Email:
-            raise ValidationError("Bitte benutze deine von der Schule verfügung gestellte Email")
-        return self.Email
-
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
